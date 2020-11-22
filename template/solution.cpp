87dad2a22059bfd798d9dc896af7d2cc556e9937
ashkelly <a.j.kelly@durham.ac.uk>
2020-11-22 20:44:35 +0000

    preparing bp code

---
+++ template/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <sstream>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <iterator>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+
+int main(){
+    int ans1 = 0, ans2 = 0;
+    std::fstream myfile("input", std::ios_base::in);
+
+
+
+    myfile.close();
+
+
+
+    std::cout << "Answer 1: " << ans1 << std::endl;
+    std::cout << "Answer 2: " << ans2 << std::endl;
+
+    return 0;
+}
