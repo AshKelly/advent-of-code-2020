f2ec794448a987caf021ac0350ee828d4785b0a6
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-04 10:20:20 +0000

    neatened up day 4

--- day4/solution.cpp
+++ day4/solution.cpp
@@ -2,38 +2,30 @@
 #include <fstream>
 
 #include <string>
-#include <sstream>
 
-#include <vector>
 #include <map>
-#include <set>
-#include <list>
 
-#include <algorithm>
 #include <iterator>
 #include <regex>
 
 
 bool valididate_byr(std::map<std::string, std::string> &documents){
     int val = std::stoi(documents["byr"]);
-    if ((val >= 1920) && (val <= 2002))
-        return true;
+    if ((val >= 1920) && (val <= 2002)) return true;
     return false;
 }
 
 
 bool valididate_eyr(std::map<std::string, std::string> &documents){
     int val = std::stoi(documents["eyr"]);
-    if ((val >= 2020) && (val <= 2030))
-        return true;
+    if ((val >= 2020) && (val <= 2030)) return true;
     return false;
 }
 
 
 bool valididate_iyr(std::map<std::string, std::string> &documents){
     int val = std::stoi(documents["iyr"]);
-    if ((val >= 2010) && (val <= 2020))
-        return true;
+    if ((val >= 2010) && (val <= 2020)) return true;
     return false;
 }
 
@@ -64,7 +56,6 @@ bool valididate_hcl(std::map<std::string, std::string> &documents){
     std::sregex_token_iterator it;
     it = {documents["hcl"].begin(), documents["hcl"].end(), reg1, 1};
     if(it != std::sregex_token_iterator()) return true;
-
     return false;
 }
 
@@ -73,7 +64,6 @@ bool valididate_ecl(std::map<std::string, std::string> &documents){
     const std::vector<std::string> ecls = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
     for (auto v : ecls)
         if (documents["ecl"] == v) return true;
-
     return false;
 }
 
@@ -83,7 +73,6 @@ bool valididate_pid(std::map<std::string, std::string> &documents){
     std::sregex_token_iterator it;
     it = {documents["pid"].begin(), documents["pid"].end(), reg1, 1};
     if(it != std::sregex_token_iterator()) return true;
-
     return false;
 }
 
@@ -92,22 +81,14 @@ int main (int argc, char *argv[]){
     int ans1 = 0, ans2 = 0;
 
     std::vector<std::string> required = {
-            "byr",
-            "iyr",
-            "eyr",
-            "hgt",
-            "hcl",
-            "ecl",
-            "pid",
+        "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid",
     };
 
     std::fstream myfile("input", std::ios_base::in);
     std::string line;
     const std::regex reg("([a-z]+):([^\\s]+)");
-
     std::map<std::string, std::string> documents;
     while(std::getline(myfile, line)){
-
         std::sregex_token_iterator it = {line.begin(), line.end(), reg, {1, 2}};
         while(it != std::sregex_token_iterator()){
             std::string k = *it;
@@ -119,11 +100,9 @@ int main (int argc, char *argv[]){
 
         if (line == ""){
             bool valid = true;
-            for(auto &k : required){
-                if (documents[k] == ""){
-                    valid = false;
-                }
-            }
+            for(auto &k : required)
+                if (documents[k] == "") valid = false;
+
             ans1 += valid;
 
             if (valid){
