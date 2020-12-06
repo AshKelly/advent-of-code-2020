fabe46090a2f93ca7e74c9f5ddadf56bf8aaf1f2
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-06 10:18:11 +0000

    formatted solutions

--- day5/solution.cpp
+++ day5/solution.cpp
@@ -1,39 +1,38 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-#include <string>
 #include <sstream>
+#include <string>
 
-#include <vector>
+#include <list>
 #include <map>
 #include <set>
-#include <list>
+#include <vector>
 
 #include <algorithm>
 #include <iterator>
 
-std::pair<int, int> find_seat(std::string boarding_pass){
+std::pair<int, int> find_seat(std::string boarding_pass) {
     int row = 0, col = 0;
     // Now we loop through the boarding pass
-    for (int i=0; i<boarding_pass.size(); i++){
-    if (boarding_pass[i] == 'F')
-        row <<= 1;
-    if (boarding_pass[i] == 'B'){
-        row <<= 1;
-	    row += 1;
-	}
-    if (boarding_pass[i] == 'L')
-        col <<= 1;
-    if (boarding_pass[i] == 'R'){
-        col <<= 1;
-	    col += 1;
-	   }
+    for (int i = 0; i < boarding_pass.size(); i++) {
+        if (boarding_pass[i] == 'F')
+            row <<= 1;
+        if (boarding_pass[i] == 'B') {
+            row <<= 1;
+            row += 1;
+        }
+        if (boarding_pass[i] == 'L')
+            col <<= 1;
+        if (boarding_pass[i] == 'R') {
+            col <<= 1;
+            col += 1;
+        }
     }
     return {row, col};
 }
 
-
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int ans1 = 0, ans2 = 0;
 
     // Load the boarding passes into a vector of strings
@@ -41,7 +40,7 @@ int main (int argc, char *argv[]){
     int sum = 0, maxv = 0, minv = 100000, id = 0;
     std::fstream myfile("input", std::ios_base::in);
     std::string line;
-    while(std::getline(myfile, line)){
+    while (std::getline(myfile, line)) {
         seat = find_seat(line);
         id = seat.first * 8 + seat.second;
 
@@ -53,7 +52,7 @@ int main (int argc, char *argv[]){
 
     // Expected sum
     ans1 = maxv;
-    int expected = (maxv*(maxv+1)) / 2 - ((minv)*(minv-1)) / 2;
+    int expected = (maxv * (maxv + 1)) / 2 - ((minv) * (minv - 1)) / 2;
     ans2 = expected - sum;
 
     std::cout << "Answer 1: " << ans1 << std::endl;
--- day6/solution.cpp
+++ day6/solution.cpp
@@ -1,36 +1,38 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-#include <string>
 #include <array>
+#include <string>
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int ans1 = 0, ans2 = 0;
 
     std::fstream myfile("input", std::ios_base::in);
     std::string line;
+
     std::array<char, 26> present;
     present.fill(0);
     int num_people = 0;
-    while(std::getline(myfile, line)){
-        if (line == ""){
+    while (std::getline(myfile, line)) {
+        if (line == "") {
             // Anyone answered yes
-            for(long unsigned int i=0; i<26; i++)
-                if (present[i] > 0) ans1 ++;
+            for (long unsigned int i = 0; i < 26; i++)
+                if (present[i] > 0)
+                    ans1++;
 
             // All answered yes
-            for(long unsigned int i=0; i<26; i++)
-                if (present[i] == num_people) ans2++;
+            for (long unsigned int i = 0; i < 26; i++)
+                if (present[i] == num_people)
+                    ans2++;
 
             // Reset counters
             present.fill(0);
             num_people = 0;
-        }
-        else{
+        } else {
             // Store reponses
             num_people++;
-            for(long unsigned int i=0; i<line.size(); i++)
-                present[int(line[i])-97]++;
+            for (long unsigned int i = 0; i < line.size(); i++)
+                present[int(line[i]) - 97]++;
         }
     }
     myfile.close();
