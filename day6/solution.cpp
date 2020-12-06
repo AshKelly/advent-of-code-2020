cea10d69a6befeb5408e55fdfb9a0eaebcac2d56
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-06 10:11:36 +0000

    day6 solved

---
+++ day6/solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <array>
+
+int main (int argc, char *argv[]){
+    int ans1 = 0, ans2 = 0;
+
+    std::fstream myfile("input", std::ios_base::in);
+    std::string line;
+    std::array<char, 26> present;
+    present.fill(0);
+    int num_people = 0;
+    while(std::getline(myfile, line)){
+        if (line == ""){
+            // Anyone answered yes
+            for(long unsigned int i=0; i<26; i++)
+                if (present[i] > 0) ans1 ++;
+
+            // All answered yes
+            for(long unsigned int i=0; i<26; i++)
+                if (present[i] == num_people) ans2++;
+
+            // Reset counters
+            present.fill(0);
+            num_people = 0;
+        }
+        else{
+            // Store reponses
+            num_people++;
+            for(long unsigned int i=0; i<line.size(); i++)
+                present[int(line[i])-97]++;
+        }
+    }
+    myfile.close();
+
+    std::cout << "Answer 1: " << ans1 << std::endl;
+    std::cout << "Answer 2: " << ans2 << std::endl;
+
+    return 0;
+}
