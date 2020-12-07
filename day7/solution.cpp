3bfe16cff56b19c80b3125ecd74c3eb8797f5191
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-07 10:59:32 +0000

    formatted d7

--- day7/solution.cpp
+++ day7/solution.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-#include <string>
 #include <sstream>
+#include <string>
 
-#include <vector>
+#include <list>
 #include <map>
 #include <set>
-#include <list>
+#include <vector>
 
 #include <algorithm>
 #include <iterator>
 #include <regex>
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int ans1 = 0, ans2 = 0;
-
 
     std::map<std::string, std::vector<std::pair<std::string, int>>> rules;
     std::set<std::string> bags;
@@ -34,7 +33,8 @@ int main (int argc, char *argv[]){
         // Then I loop through all of the bags it contains
         std::string contains = pieces_match[2];
         std::regex reg("(\\d+) ([a-z\\s]+) bag");
-        std::sregex_token_iterator it = {contains.begin(), contains.end(), reg, {1, 2}};
+        std::sregex_token_iterator it = {
+            contains.begin(), contains.end(), reg, {1, 2}};
         while (it != std::sregex_token_iterator()) {
             int num = std::stoi(*it);
             it = std::next(it);
@@ -48,22 +48,22 @@ int main (int argc, char *argv[]){
     myfile.close();
 
     // Part 1: how many different colour outer bags, contain a shiny gold bag?
-    for(std::string ob : bags){
+    for (std::string ob : bags) {
         if (ob == "shiny gold")
             continue;
 
         std::list<std::string> possible_bags = {ob};
-        while (possible_bags.size() > 0){
+        while (possible_bags.size() > 0) {
             std::string b = possible_bags.front();
 
             // We found gold bag, lets move on
-            if (b == "shiny gold"){
+            if (b == "shiny gold") {
                 ans1++;
                 break;
             }
 
             // Add extra bags to check
-            for(auto kv : rules[b]){
+            for (auto kv : rules[b]) {
                 possible_bags.push_back(kv.first);
             }
 
@@ -74,13 +74,13 @@ int main (int argc, char *argv[]){
     // Part 2: how many bags in a shiny gold bag?
     std::list<std::pair<std::string, int>> possible_bags;
     possible_bags.push_back({"shiny gold", 1});
-    while (possible_bags.size() > 0){
+    while (possible_bags.size() > 0) {
         std::pair<std::string, int> b = possible_bags.front();
         ans2 += b.second;
 
         // Add extra bags to check
-        for(auto kv : rules[b.first]){
-            possible_bags.push_back({kv.first, kv.second*b.second});
+        for (auto kv : rules[b.first]) {
+            possible_bags.push_back({kv.first, kv.second * b.second});
         }
 
         possible_bags.pop_front();
