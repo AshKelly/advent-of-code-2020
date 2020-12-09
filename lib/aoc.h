e7b3571b0810221994775037ff52daf94b5e39e6
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-09 17:36:33 +0000

    Added pre-compiled headers and switched to c++11 due to faster compile times

---
+++ lib/aoc.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <sstream>
+
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+
+#include <algorithm>
+#include <iterator>
