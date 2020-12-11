ab5fca41c32cab9fd615cc297c9ba3babf0078fc
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-11 15:14:04 +0000

    updated solution to faster technique

--- day9/solution.cpp
+++ day9/solution.cpp
@@ -1,42 +1,52 @@
 #include "../lib/aoc.h"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
+    auto begin = std::chrono::high_resolution_clock::now();
     long int ans1 = 0, ans2 = 0;
-    size_t preamble_max_size = 25;
+    size_t preamble_max_size = 1000;
 
     std::fstream myfile("input", std::ios_base::in);
     std::string line;
     std::list<long int> preamble;
     std::list<long int>::iterator it;
-    std::map<long int, long int> cached;
+    std::unordered_map<long int, int> cached;
+    std::unordered_map<long int, int>::iterator cached_it;
+
     while (std::getline(myfile, line)) {
         long int num = std::stol(line);
 
-        // Break if value not in cache
         if (preamble.size() == preamble_max_size) {
-            if (cached[num] == 0) {
+            // Can we find the pair of numbers which sum?
+            bool found = false;
+            it = preamble.begin();
+            while (it != preamble.end()) {
+                cached_it = cached.find(num - *it);
+                if (cached_it != cached.end()) found = true;
+                it = std::next(it);
+            }
+
+            if (found == false) {
                 ans1 = num;
                 break;
             }
 
-            // Remove oldest value from preamble, and it's contributions to the
-            // cache
+            // Remove oldest value from preamble
             int rem = preamble.front();
             preamble.pop_front();
-            it = preamble.begin();
-            while (it != preamble.end()) {
-                cached[*it + rem]--;
-                it = std::next(it);
+
+            // remove value from cache
+            cached_it = cached.find(rem);
+            if (cached_it != cached.end()){
+                if ((*cached_it).second == 1)
+                    cached.erase(cached_it);
+                else{
+                    (*cached_it).second--;
+                }
             }
         }
 
-        // Add new values contribution to cache, and then insert the new value
-        // to preamble
-        it = preamble.begin();
-        while (it != preamble.end()) {
-            cached[*it + num]++;
-            it = std::next(it);
-        }
+        cached[num]++;
         preamble.push_back(num);
     }
     myfile.clear();
@@ -77,6 +87,9 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Answer 1: " << ans1 << std::endl;
     std::cout << "Answer 2: " << ans2 << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    double runtime = (double) std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+    std::cout << runtime/1.e3 << " milli seconds" << std::endl;
 
     return 0;
 }
