14c1e168915345a29aef672570db5c60c0918db6
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-17 14:27:08 +0000

    two d17 versions

--- day17/solution.cpp
+++ day17/solution.cpp
@@ -1,25 +1,38 @@
 #include "../lib/aoc.h"
 
-void get_active_range(std::map<std::array<int, 4>, char> &world, std::array<int, 8> &range){
-    range.fill(0);
-    for(auto kv : world){
-        range[0] = std::min(kv.first[0]-1, range[0]);
-        range[1] = std::max(kv.first[0]+1, range[1]);
-        
-        range[2] = std::min(kv.first[1]-1, range[2]);
-        range[3] = std::max(kv.first[1]+1, range[3]);
+const int MAX_IND = 1000;
+const int HALF = 500;
 
-        range[4] = std::min(kv.first[2]-1, range[4]);
-        range[5] = std::max(kv.first[2]+1, range[5]);
-        
-        range[6] = std::min(kv.first[3]-1, range[6]);
-        range[7] = std::max(kv.first[3]+1, range[7]);
-    }
+void get_xyzw(long int key, int &x, int &y, int &z, int &w){
+    w = key % MAX_IND - HALF;
+    key -= w;
+    key /= MAX_IND;
+
+    z = key % MAX_IND - HALF;
+    key -= z;
+    key /= MAX_IND;
+
+    y = key % MAX_IND - HALF;
+    key -= y;
+    key /= MAX_IND;
+
+    x = key - HALF;
+}
+
+long int key_xyzw(int x, int y, int z, int w){
+    long int key = x+HALF;
+    key *= MAX_IND;
+    key += y+HALF;
+    key *= MAX_IND;
+    key += z+HALF;
+    key *= MAX_IND;
+    key += w+HALF;
+    return key;
 }
 
 
-int count_active_neighbours(std::map<std::array<int, 4>, char> &world, int x, int y, int z, int w){
-    std::map<std::array<int, 4>, char>::iterator it;
+int count_active_neighbours(std::unordered_map<long int, char> &world, int x, int y, int z, int w){
+    std::unordered_map<long int, char>::iterator it;
     int active = 0;
     for(int xi=x-1; xi<=x+1; xi++)
         for(int yi=y-1; yi<=y+1; yi++)
@@ -27,7 +40,8 @@ int count_active_neighbours(std::map<std::array<int, 4>, char> &world, int x, in
                 for(int wi=w-1; wi<=w+1; wi++){
                     if((xi == x) && (yi == y) && (zi == z) && (wi == w))
                         continue;
-                    it = world.find({xi, yi, zi, wi});
+
+                    it = world.find(key_xyzw(xi, yi, zi, wi));
                     if (it != world.end())
                         active++;
                 }
@@ -36,45 +50,47 @@ int count_active_neighbours(std::map<std::array<int, 4>, char> &world, int x, in
 }
 
 int main (int argc, char *argv[]){
+    const int num_cycles = 6;
     int ans1 = 0, ans2 = 0;
 
-
-    std::map<std::array<int, 4>, char> world_initial;
     std::fstream myfile("input", std::ios_base::in);
     std::string line;
     int lineno = 0;
+    std::unordered_map<long int, char> world_initial;
     while(std::getline(myfile, line)){
         for(size_t colno=0; colno<line.size(); colno++)
-            if (line[colno] == '#') 
-                world_initial[{lineno, int(colno), 0, 0}] = line[colno];
+            if (line[colno] == '#'){
+                world_initial[key_xyzw(lineno, colno, 0, 0)] = 1;
+            }
         lineno++;
     }
     myfile.close();
 
     // Part 1
-    std::map<std::array<int, 4>, char> world = world_initial;
-    std::map<std::array<int, 4>, char>::iterator it;
     std::array<int, 8> range;
-    int num_cycles = 6;
+    range.fill(0);
+    range[1] = 8;
+    range[3] = 8;
+
+    std::unordered_map<long int, char> world = world_initial;
+    std::unordered_map<long int, char>::iterator it;
     for(int cycle=1; cycle<=num_cycles; cycle++){
-        get_active_range(world, range);
-        std::map<std::array<int, 4>, char> new_world;
+        std::unordered_map<long int, char> new_world;
 
         int w = 0;
         int new_active = 0;
-        for(int x=range[0]; x<=range[1]; x++) 
-            for(int y=range[2]; y<=range[3]; y++) 
-                for(int z=range[4]; z<=range[5]; z++){
-                    it = world.find({x, y, z, w});
+        for(int x=range[0]-cycle; x<=range[1]+cycle; x++)
+            for(int y=range[2]-cycle; y<=range[3]+cycle; y++)
+                for(int z=range[4]-cycle; z<=range[5]+cycle; z++){
+                    long int key = key_xyzw(x, y, z, w);
                     int active = count_active_neighbours(world, x, y, z, w);
-                    // Active remain active
-                    if ((it != world.end()) && ((active == 3) || (active == 2))){
-                        new_world[{x, y, z, w}] = '#';
-                        new_active++;
-                    }
-                    // Inactive to active
-                    if ((it == world.end()) && (active == 3)){
-                        new_world[{x, y, z, w}] = '#';
+                    it = world.find(key);
+                    if (
+                        (it != world.end()) && ((active == 3) || (active == 2))
+                            ||
+                        ((it == world.end()) && (active == 3))
+                        ){
+                        new_world[key] = 1;
                         new_active++;
                     }
                 }
@@ -82,28 +98,29 @@ int main (int argc, char *argv[]){
         world = new_world;
         ans1 = new_active;
     }
-    
+
     // Part 2
+    range.fill(0);
+    range[1] = 8;
+    range[3] = 8;
+
     world = world_initial;
     for(int cycle=1; cycle<=num_cycles; cycle++){
-        get_active_range(world, range);
-        std::map<std::array<int, 4>, char> new_world;
-
+        std::unordered_map<long int, char> new_world;
         int new_active = 0;
-        for(int x=range[0]; x<=range[1]; x++) 
-            for(int y=range[2]; y<=range[3]; y++) 
-                for(int z=range[4]; z<=range[5]; z++)
-                    for(int w=range[6]; w<=range[7]; w++){
-                        it = world.find({x, y, z, w});
+        for(int x=range[0]-cycle; x<=range[1]+cycle; x++)
+            for(int y=range[2]-cycle; y<=range[3]+cycle; y++)
+                for(int z=range[4]-cycle; z<=range[5]+cycle; z++)
+                    for(int w=range[6]-cycle; w<=range[7]+cycle; w++){
+                        long int key = key_xyzw(x, y, z, w);
                         int active = count_active_neighbours(world, x, y, z, w);
-                        // Active remain active
-                        if ((it != world.end()) && ((active == 3) || (active == 2))){
-                            new_world[{x, y, z, w}] = '#';
-                            new_active++;
-                        }
-                        // Inactive to active
-                        if ((it == world.end()) && (active == 3)){
-                            new_world[{x, y, z, w}] = '#';
+                        it = world.find(key);
+                        if (
+                            (it != world.end()) && ((active == 3) || (active == 2))
+                                ||
+                            ((it == world.end()) && (active == 3))
+                            ){
+                            new_world[key] = 1;
                             new_active++;
                         }
                 }
---
+++ day17/solution_ONM.cpp
@@ -0,0 +1,136 @@
+#include "../lib/aoc.h"
+
+const int MAX_IND = 1000;
+const int HALF = 500;
+
+void get_xyzw(long int key, int &x, int &y, int &z, int &w) {
+    w = key % MAX_IND - HALF;
+    key -= w;
+    key /= MAX_IND;
+
+    z = key % MAX_IND - HALF;
+    key -= z;
+    key /= MAX_IND;
+
+    y = key % MAX_IND - HALF;
+    key -= y;
+    key /= MAX_IND;
+
+    x = key - HALF;
+}
+
+long int key_xyzw(int x, int y, int z, int w) {
+    long int key = x + HALF;
+    key *= MAX_IND;
+    key += y + HALF;
+    key *= MAX_IND;
+    key += z + HALF;
+    key *= MAX_IND;
+    key += w + HALF;
+    return key;
+}
+
+int count_active_neighbours(std::unordered_map<long int, char> &world, int x,
+                            int y, int z, int w) {
+    std::unordered_map<long int, char>::iterator it;
+    int active = 0;
+    for (int xi = x - 1; xi <= x + 1; xi++)
+        for (int yi = y - 1; yi <= y + 1; yi++)
+            for (int zi = z - 1; zi <= z + 1; zi++)
+                for (int wi = w - 1; wi <= w + 1; wi++) {
+                    if ((xi == x) && (yi == y) && (zi == z) && (wi == w))
+                        continue;
+
+                    it = world.find(key_xyzw(xi, yi, zi, wi));
+                    if (it != world.end())
+                        active++;
+                }
+
+    return active;
+}
+
+int main(int argc, char *argv[]) {
+    const int num_cycles = 6;
+    int ans1 = 0, ans2 = 0;
+
+    std::fstream myfile("input", std::ios_base::in);
+    std::string line;
+    int lineno = 0;
+    std::unordered_map<long int, char> world_initial;
+    while (std::getline(myfile, line)) {
+        for (size_t colno = 0; colno < line.size(); colno++)
+            if (line[colno] == '#') {
+                world_initial[key_xyzw(lineno, colno, 0, 0)] = 1;
+            }
+        lineno++;
+    }
+    myfile.close();
+
+    std::unordered_map<long int, char> world = world_initial;
+    std::unordered_map<long int, char>::iterator it;
+    int x, y, z, w;
+    for (int cycle = 1; cycle <= num_cycles; cycle++) {
+        std::unordered_map<long int, char> new_world;
+        int new_active = 0;
+
+        for(auto cube : world){
+            get_xyzw(cube.first, x, y, z, w);
+
+            for (int xi = x - 1; xi <= x + 1; xi++)
+                for (int yi = y - 1; yi <= y + 1; yi++)
+                    for (int zi = z - 1; zi <= z + 1; zi++){
+                        long int key = key_xyzw(xi, yi, zi, 0);
+                        it = new_world.find(key);
+                        if (it != new_world.end()) continue;
+
+                        int active = count_active_neighbours(world, xi, yi, zi, 0);
+
+                        it = world.find(key);
+                        if ((it != world.end()) && ((active == 3) || (active == 2)) ||
+                            ((it == world.end()) && (active == 3))) {
+                            new_world[key] = 1;
+                            new_active++;
+                        }
+                    }
+        }
+
+        world = new_world;
+        ans1 = new_active;
+    }
+
+    world = world_initial;
+    for (int cycle = 1; cycle <= num_cycles; cycle++) {
+        std::unordered_map<long int, char> new_world;
+        int new_active = 0;
+
+        for(auto cube : world){
+            get_xyzw(cube.first, x, y, z, w);
+
+            for (int xi = x - 1; xi <= x + 1; xi++)
+                for (int yi = y - 1; yi <= y + 1; yi++)
+                    for (int zi = z - 1; zi <= z + 1; zi++)
+                        for (int wi = w - 1; wi <= w + 1; wi++){
+                            long int key = key_xyzw(xi, yi, zi, wi);
+                            it = new_world.find(key);
+                            if (it != new_world.end()) continue;
+
+                            int active = count_active_neighbours(world, xi, yi, zi, wi);
+
+                            it = world.find(key);
+                            if ((it != world.end()) && ((active == 3) || (active == 2)) ||
+                                ((it == world.end()) && (active == 3))) {
+                                new_world[key] = 1;
+                                new_active++;
+                            }
+                        }
+        }
+
+        world = new_world;
+        ans2 = new_active;
+    }
+
+    std::cout << "Answer 1: " << ans1 << std::endl;
+    std::cout << "Answer 2: " << ans2 << std::endl;
+
+    return 0;
+}
