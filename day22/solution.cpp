950f56ebdf212566b720a3fdfc66821c571a1ca3
Ashley Kelly <a.j.kelly@durham.ac.uk>
2020-12-23 13:54:15 +0000

    d22 and d23 formatted

--- day22/solution.cpp
+++ day22/solution.cpp
@@ -2,18 +2,16 @@
 
 int calculate_score(std::list<int> &deck) {
     int score = 0;
-    std::list<int>::reverse_iterator it = deck.rbegin();
     int i = 1;
+    std::list<int>::reverse_iterator it = deck.rbegin();
     while (it != deck.rend()) {
-        score += i * (*it);
-        i++;
+        score += (i++) * (*it);
         it = std::next(it);
     }
     return score;
 }
 
 int play_game1(std::list<int> deck1, std::list<int> deck2) {
-    int score = 0;
     while ((deck1.size() > 0) && (deck2.size() > 0)) {
         int v1 = deck1.front();
         int v2 = deck2.front();
@@ -28,9 +26,11 @@ int play_game1(std::list<int> deck1, std::list<int> deck2) {
             deck2.push_back(v1);
         }
     }
-    score += calculate_score(deck1);
-    score += calculate_score(deck2);
-    return score;
+
+    if (deck1.size() > 0)
+        return calculate_score(deck1);
+    else
+        return -calculate_score(deck2);
 }
 
 int play_recursive_combat(std::list<int> deck1, std::list<int> deck2) {
@@ -38,11 +38,12 @@ int play_recursive_combat(std::list<int> deck1, std::list<int> deck2) {
     std::map<std::list<int>, int> deck2_history = {};
 
     while ((deck1.size() > 0) && (deck2.size() > 0)) {
-        if ((deck1_history[deck1] > 0) && (deck2_history[deck2] > 0)) {
+        if ((deck1_history.find(deck1) != deck1_history.end()) &&
+            (deck2_history.find(deck2) != deck2_history.end())) {
             return calculate_score(deck1);
         }
-        deck1_history[deck1]++;
-        deck2_history[deck2]++;
+        deck1_history.insert({deck1, 1});
+        deck2_history.insert({deck2, 1});
 
         int v1 = deck1.front();
         int v2 = deck2.front();
@@ -53,18 +54,31 @@ int play_recursive_combat(std::list<int> deck1, std::list<int> deck2) {
             std::list<int> new_deck1 = {};
             std::list<int> new_deck2 = {};
 
+            int max_val = 0;
+            int max_holder = 0;
             std::list<int>::iterator it = deck1.begin();
             for (int i = 0; i < v1; i++) {
                 new_deck1.push_back(*it);
-                it = std::next(it);
-            }
-            it = deck2.begin();
-            for (int i = 0; i < v2; i++) {
-                new_deck2.push_back(*it);
+                max_val = std::max(*it, max_val);
                 it = std::next(it);
             }
 
-            int winner = play_recursive_combat(new_deck1, new_deck2);
+            it = deck2.begin();
+            for (int i = 0; i < v2; i++) {
+                new_deck2.push_back(*it);
+                if (*it > max_val) {
+                    max_holder = 1;
+                }
+                it = std::next(it);
+            }
+
+            // In player 1 has the largest number in their deck then there is
+            // no way they can lose a recursive game... If they player 2 has
+            // the largest number they could lose due to the infinite loop rule
+            int winner = 0;
+            if (max_holder == 1)
+                winner = play_recursive_combat(new_deck1, new_deck2);
+
             if (winner >= 0) {
                 deck1.push_back(v1);
                 deck1.push_back(v2);
--- day23/solution.cpp
+++ day23/solution.cpp
@@ -1,41 +1,41 @@
 #include "../lib/aoc.h"
 
 class Cup {
-    public:
-        Cup * next;
-        int value;
+  public:
+    Cup *next;
+    int value;
 
-        Cup (int v) : value(v) {};
-        void set_next(Cup * n){
-            next = n;
-        };
+    Cup(int v) : value(v){};
+    void set_next(Cup *n) {
+        next = n;
+    };
 };
 
-std::vector<Cup> solve(std::vector<int> order, int num_cups, int reps){
+std::vector<Cup> solve(std::vector<int> order, int num_cups, int reps) {
     std::vector<Cup> cups;
-    for(int i=1; i<num_cups; i++)
+    for (int i = 1; i < num_cups; i++)
         cups.push_back(Cup(i));
 
     // Set up pointers to match initial order
-    Cup * temp;
-    for(int i=0; i<order.size()-1; i++){
-        temp = &cups[order[i]-1];
-        temp->set_next(&cups[order[i+1]-1]);
+    Cup *temp;
+    for (int i = 0; i < order.size() - 1; i++) {
+        temp = &cups[order[i] - 1];
+        temp->set_next(&cups[order[i + 1] - 1]);
     }
-    cups[order[order.size()-1]-1].set_next(&cups[order[0]-1]);
+    cups[order[order.size() - 1] - 1].set_next(&cups[order[0] - 1]);
 
     // Add pointers between extra values
-    if (num_cups > order.size()+1){
-        int last = order[order.size()-1]-1;
+    if (num_cups > order.size() + 1) {
+        int last = order[order.size() - 1] - 1;
         cups[last].set_next(&cups[order.size()]);
-        for(size_t i=order.size(); i<cups.size()-1; i++)
-            cups[i].set_next(&cups[i+1]);
-        cups[cups.size()-1].set_next(&cups[order[0]-1]);
+        for (size_t i = order.size(); i < cups.size() - 1; i++)
+            cups[i].set_next(&cups[i + 1]);
+        cups[cups.size() - 1].set_next(&cups[order[0] - 1]);
     }
 
-    Cup * curr = &cups[0];
-    Cup * start, * end, * destination;
-    for(int i=0; i<reps; i++){
+    Cup *curr = &cups[0];
+    Cup *start, *end, *destination;
+    for (int i = 0; i < reps; i++) {
         start = curr->next;
 
         int target = curr->value - 1;
@@ -44,18 +44,18 @@ std::vector<Cup> solve(std::vector<int> order, int num_cups, int reps){
 
         // Select pickup
         end = curr;
-        for(int j=0; j<3; j++){
+        for (int j = 0; j < 3; j++) {
             end = end->next;
         }
 
         // Find destination cup by ensuring our target is not in the pickup
         bool found = false;
-        while (!found){
+        while (!found) {
             temp = start;
             found = true;
 
-            for(int j=0; j<3; j++){
-                if (temp->value == target){
+            for (int j = 0; j < 3; j++) {
+                if (temp->value == target) {
                     target--;
                     if (target == 0)
                         target = cups.size();
@@ -68,7 +68,7 @@ std::vector<Cup> solve(std::vector<int> order, int num_cups, int reps){
         }
 
         // Every cup is in order!
-        destination = &cups[target-1];
+        destination = &cups[target - 1];
 
         // Remove the elements
         curr->next = end->next;
@@ -82,11 +82,10 @@ std::vector<Cup> solve(std::vector<int> order, int num_cups, int reps){
         curr = curr->next;
     }
 
-
     return cups;
 }
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     long int ans1 = 0, ans2 = 1;
 
     std::vector<int> order = {1, 9, 3, 4, 6, 7, 2, 5, 8};
@@ -94,10 +93,10 @@ int main (int argc, char *argv[]){
     std::vector<Cup> cups2 = solve(order, 1000001, 10000000);
 
     /* Print all the numbers after cup 1 */
-    Cup * start = &cups1[0];
+    Cup *start = &cups1[0];
     std::cout << "Answer 1: ";
     start = start->next;
-    for(int j=0; j<8; j++){
+    for (int j = 0; j < 8; j++) {
         std::cout << start->value;
         start = start->next;
     }
@@ -105,7 +104,7 @@ int main (int argc, char *argv[]){
 
     /* Multiply two numbers afterwards */
     start = &cups2[0];
-    for(int j=0; j<3; j++){
+    for (int j = 0; j < 3; j++) {
         ans2 *= start->value;
         start = start->next;
     }
